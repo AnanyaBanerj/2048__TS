56c9fd6a036b0478b7e3a9ed71de8903f3cc63e5
AnanyaBanerjee <89412513+AnanyaBanerj@users.noreply.github.com>
2023-10-17 00:17:32 +0530

    suggested updates

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,29 +1,23 @@
-const mongoose  = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/LoginSignup")
-.then(()=>{
-    console.log("mongodb connected");
-})
-
-.catch(()=>{
-    console.log("failed");
-})
-
-
-
+mongoose.connect("mongodb://localhost:27017/LoginSignup").then(() => {
+  console.log("mongodb connected");
+}).catch((error) => {
+  console.error("MongoDB connection failed:", error);
+});
 
 const userSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  });
-  
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false, // Hide password field from query results
+  },
+});
 
-const collections = new mongoose.model('Collections',userSchema)
+const collections = mongoose.model('Collections', userSchema);
 
-module.exports= collections
+module.exports = collections;
